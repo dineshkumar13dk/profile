c366827c7e3d175854a31393b6df0f5ee60e4e54
S DINESH KUMAR <132070584+dineshkumar13dk@users.noreply.github.com>
2024-12-08 17:04:53 +0530

    sta updated

--- dineshkumar/resources/script.js
+++ dineshkumar/resources/script.js
@@ -11,3 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
     // Format the time and display
     lastUpdatedElement.textContent = now.toLocaleString("en-US", options);
 });
+
+document.querySelectorAll('.dropdown-btn').forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const description = e.target.closest('figure').querySelector('.image-description');
+        const isHidden = description.style.display === 'none' || description.style.display === '';
+        description.style.display = isHidden ? 'block' : 'none';
+        btn.classList.toggle('rotate', isHidden);
+    });
+});
