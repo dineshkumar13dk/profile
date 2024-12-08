0b045dd3fc3dc03c2708c69243e289d2c6a9cc3e
S DINESH KUMAR <132070584+dineshkumar13dk@users.noreply.github.com>
2024-12-08 09:12:50 +0530

    script updated

--- dineshkumar/resources/script.js
+++ dineshkumar/resources/script.js
@@ -1,23 +1,3 @@
-// Sidebar Toggle Functionality
-const sidebar = document.getElementById("sidebar");
-const toggleButton = document.getElementById("toggle-button");
-const toggleIcon = document.getElementById("toggle-icon");
-const mainContent = document.querySelector(".main-content");
-
-toggleButton.addEventListener("click", () => {
-    sidebar.classList.toggle("closed");
-    mainContent.classList.toggle("expanded");
-
-    // Update icon
-    if (sidebar.classList.contains("closed")) {
-        toggleIcon.classList.remove("fa-arrow-left");
-        toggleIcon.classList.add("fa-arrow-right");
-    } else {
-        toggleIcon.classList.remove("fa-arrow-right");
-        toggleIcon.classList.add("fa-arrow-left");
-    }
-});
-
 document.addEventListener("DOMContentLoaded", () => {
     const lastUpdatedElement = document.getElementById("last-updated-time");
 
